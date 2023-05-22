a9c14a7e5f3d3d950ef332aa1387890228a95a3a
Mateus <mateus.eneas98@gmail.com>
2023-05-22 11:40:02 -0300

    Versão muito ruim de solucionar exceptions

--- src/main/java/entities/Reserva.java
+++ src/main/java/entities/Reserva.java
@@ -1,4 +1,62 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reserva {
+
+    private Integer numeroQuarto;
+    private Date dataEntrada;
+    private Date dataSaida;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reserva() {
+    }
+
+    public Reserva(Integer numeroQuarto, Date dataEntrada, Date dataSaida) {
+        this.numeroQuarto = numeroQuarto;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    public Integer getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public void setNumeroQuarto(Integer numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public Date getDataSaida() {
+        return dataSaida;
+    }
+
+    public long duration() {
+        long diff = dataSaida.getTime() - dataEntrada.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void alterarData (Date dataEntrada, Date dataSaida) {
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    @Override
+    public String toString() {
+        return "Quarto "
+                + numeroQuarto
+                + ", data de entrada: "
+                + sdf.format(dataEntrada)
+                + ", data de saída: "
+                + sdf.format(dataSaida)
+                + ", "
+                + duration()
+                + " noites";
+    }
 }
--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -1,8 +1,52 @@
 package program;
 
+import entities.Reserva;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Numero do quarto: ");
+        int numero = sc.nextInt();
+        System.out.print("Data de entrada (DD/MM/YYYY): ");
+        Date dataEntrada = sdf.parse(sc.next());
+        System.out.print("Data de saida (DD/MM/YYYY): ");
+        Date dataSaida = sdf.parse(sc.next());
+
+        if (!dataSaida.after(dataEntrada)) {
+            System.out.println("Erro na reserva: Data de entrada não pode ser depois da data de saída!");
+        }
+        else {
+            Reserva reserva = new Reserva(numero, dataEntrada, dataSaida);
+            System.out.println("Reserva: " + reserva);
+
+            System.out.println();
+            System.out.println("Entre com a alteração da data: ");
+            System.out.print("Data de entrada (DD/MM/YYYY): ");
+            dataEntrada = sdf.parse(sc.next());
+            System.out.print("Data de saida (DD/MM/YYYY): ");
+            dataSaida = sdf.parse(sc.next());
+
+            Date agora = new Date();
+            if (dataEntrada.before(agora) || dataSaida.before(agora)) {
+                System.out.println("A data de reserva tem que ser futura!");
+            }
+            else if (!dataSaida.after(dataEntrada)) {
+                System.out.println("Erro na reserva: Data de entrada não pode ser depois da data de saída!");
+            }
+            else {
+                reserva.alterarData(dataEntrada, dataSaida);
+                System.out.println("Reserva: " + reserva);
+            }
+        }
+        sc.close();
     }
 }
